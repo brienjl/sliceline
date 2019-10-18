64d9355588e9212165637505a3da228ab9dfb50a
Jeremy Brien <brienjl@gmail.com>
2019-10-18 15:40:23 -0700

    banner added

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,6 +6,8 @@ import {Title} from "../Styles/title";
 const NavbarStyled = styled.div`
 background-color: ${pizzaRed};
 padding: 10px;
+position: fixed;
+width: 100%;
 `
 const Logo = styled(Title)`
     font-size: 20px;
@@ -16,7 +18,7 @@ const Logo = styled(Title)`
 export function Navbar(){
     return <NavbarStyled>
         <Logo>
-           Sliceline  🍕 
+           Sliceline
         </Logo>
     </NavbarStyled>;
 }
