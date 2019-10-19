9ef4a3e2261550ee2d1563be0538f6eb5be11289
Jeremy Brien <brienjl@gmail.com>
2019-10-18 21:59:11 -0700

    corrected url reference for banner image

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const Banner = styled.div`
+{
     height: 200px;
-    background-image: url("/public/img/banner.jpg");
+    background-image: url('img/banner.jpg');
     background-position: center;
     background-size: cover;
+}
 `
