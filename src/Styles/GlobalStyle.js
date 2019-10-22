02f865dcb8f7ad42dc329bbbf6e89e305606678e
Jeremy Brien <brienjl@gmail.com>
2019-10-22 15:15:38 -0700

    refactored globalStyle

---
+++ src/Styles/GlobalStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const GlobalStyle = styled.div`
+  body {
+    margin: 0;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
